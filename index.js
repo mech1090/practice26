54ff8e067edc9a6bdc64546b7747917bf26353b9
Shahzad <mech1090@gmail.com>
2022-07-16 10:38:40 -0600

    skelaton complete

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const config = require('config')
 const mongoose = require('mongoose')
+const userPage = require('./route/user.route')
+require('./db')
 
 const app = express()
 
@@ -10,6 +12,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}))
+app.use('/user',userPage)
+
+app.set('view engine','pug')
+app.set('views','./views')
 
 app.get('/',(req,res)=>{
     res.send('Testing')
@@ -18,7 +24,9 @@ app.get('/',(req,res)=>{
 app.get('*',(req,res)=>{
     res.send('BAD_REQUEST')
 })
-
-app.listen(config.get('port'),()=>{
-    console.log(`Server is running on port ${config.get('port')}`)
+mongoose.connection.once('open',()=>{
+    app.listen(config.get('port'),()=>{
+        console.log(`Server is running on port ${config.get('port')}`)
+    })
+    console.log('DB CONNECTED')
 })
